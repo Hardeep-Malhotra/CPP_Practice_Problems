9bed5733d133f8e92b6b5cdc6b30201da6cb9754
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-02-09 20:43:52 +0530

    C++ program to convert int to char using typecasting

---
+++ C++ Conversion Programs/Int to Char.cpp
@@ -0,0 +1,13 @@
+// C++ program to convert 
+// int to char using typecasting 
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main() 
+{
+  int N = 65; 
+  char c = N;
+  cout << c;
+  return 0;
+}
