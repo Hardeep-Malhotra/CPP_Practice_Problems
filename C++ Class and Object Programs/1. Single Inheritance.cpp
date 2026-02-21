d5dc850bf17f75f4c192dfed19cf15194ffbd695
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-02-21 20:23:01 +0530

    Fix typo in comment for single inheritance explanation

--- C++ Class and Object Programs/1. Single Inheritance.cpp
+++ C++ Class and Object Programs/1. Single Inheritance.cpp
@@ -1,4 +1,4 @@
-// In single inheritance, a sub-class is derived from only one super class. It inherits the properties and behavior of a single-parent class. Sometimes, it is also known as simple inheritance.
+// In single inharitance, a sub-class is derived from only one super class. It inherits the properties and behavior of a single-parent class. Sometimes, it is also known as simple inheritance.
 
 #include <iostream>
 using namespace std;
